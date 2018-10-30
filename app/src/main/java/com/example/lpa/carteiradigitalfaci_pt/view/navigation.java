f89f5dc44237736edd5755eaa859eecf6229e2f7
Anderson Lobato <xd.anderson4@gmail.com>
2018-10-30 19:53:41 -0300

    Criação dos pacotes MVC Criação da classe Modelo "Documentos" Ajustes de interface gráfica

--- app/src/main/java/com/example/lpa/carteiradigitalfaci_pt/view/navigation.java
+++ app/src/main/java/com/example/lpa/carteiradigitalfaci_pt/view/navigation.java
@@ -69,7 +69,8 @@ public class navigation extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sair) {
+            finish();
             return true;
         }
 
